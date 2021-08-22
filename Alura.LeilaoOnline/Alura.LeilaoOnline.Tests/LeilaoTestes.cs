15e31c7a64ef2c6f7df130ee1bbe9be00d53cde4
medeiros13 <gabrielmedeiros13122000@gmail.com>
2021-08-22 19:00:05 -0300

    Refatorado o código, agora o LeilaoComVariosLances abrange vários testes que tem o mesmo valor esperado, o que muda são os valores de entrada.

--- Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTestes.cs
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTestes.cs
@@ -8,6 +8,33 @@ namespace Alura.LeilaoOnline.Tests
 {
     public class LeilaoTestes
     {
+
+        [Theory]
+        [InlineData(1200, new double[] { 800, 900, 1000, 1200 })] //Teste com os lances ordenados com valor
+        [InlineData(1000, new double[] { 800, 900, 1000, 990 })] //Teste com os lances desordenados por valor
+        [InlineData(800, new double[] { 800 })] //Teste com apenas um lance
+        public void LeilaoComVariosLances(
+            double valorEsperado,
+            double[] ofertas)
+        {
+            //Arrange - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+
+            foreach (var valor in ofertas)
+            {
+                leilao.RecebeLance(fulano, valor);
+            }
+
+            //Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert - verificação dos critérios de aceitação (verifica se o Act foi bem sucedido)
+            var valorObtido = leilao.Ganhador.Valor;
+            Assert.Equal(valorEsperado, valorObtido);
+
+        }
+                                                                                                    
         [Fact]
         public void LeilaoSemLances()
         {
@@ -19,96 +46,6 @@ namespace Alura.LeilaoOnline.Tests
 
             //Assert - verificação dos critérios de aceitação (verifica se o Act foi bem sucedido)
             var valorEsperado = 0;
-            var valorObtido = leilao.Ganhador.Valor;
-
-            Assert.Equal(valorEsperado, valorObtido);
-        }
-        [Fact]
-        public void LeilaoComTresClientes()
-        {
-            //Arrange - cenário
-            var leilao = new Leilao("Van Gogh");
-            var fulano = new Interessada("Fulano", leilao);
-            var maria = new Interessada("Maria", leilao);
-            var beltrano = new Interessada("Beltrano", leilao);
-
-            leilao.RecebeLance(fulano, 800);
-            leilao.RecebeLance(maria, 900);
-            leilao.RecebeLance(maria, 990);
-            leilao.RecebeLance(fulano, 1000);
-            leilao.RecebeLance(beltrano, 1400);
-
-            //Act - método sob teste
-            leilao.TerminaPregao();
-
-            //Assert - verificação dos critérios de aceitação (verifica se o Act foi bem sucedido)
-            var valorEsperado = 1400;
-            var valorObtido = leilao.Ganhador.Valor;
-
-            Assert.Equal(valorEsperado, valorObtido);
-            Assert.Equal(beltrano, leilao.Ganhador.Cliente);
-        }
-        [Fact]
-        public void LeilaoComlancesOrdenadosPorValor()
-        {
-            //Arrange - cenário
-            var leilao = new Leilao("Van Gogh");
-            var fulano = new Interessada("Fulano", leilao);
-            var maria = new Interessada("Maria", leilao);
-
-            leilao.RecebeLance(fulano, 800);
-            leilao.RecebeLance(maria, 900);
-            leilao.RecebeLance(maria, 990);
-            leilao.RecebeLance(fulano, 1000);
-
-            //Act - método sob teste
-            leilao.TerminaPregao();
-
-            //Assert - verificação dos critérios de aceitação (verifica se o Act foi bem sucedido)
-            var valorEsperado = 1000;
-            var valorObtido = leilao.Ganhador.Valor;
-
-            Assert.Equal(valorEsperado, valorObtido);
-        }
-
-        [Fact]
-        public void LeilaoComVariosLances()
-        {
-            //Arrange - cenário
-            var leilao = new Leilao("Van Gogh");
-            var fulano = new Interessada("Fulano", leilao);
-            var maria = new Interessada("Maria", leilao);
-
-            leilao.RecebeLance(fulano, 800);
-            leilao.RecebeLance(maria, 900);
-            leilao.RecebeLance(fulano, 1000);
-            leilao.RecebeLance(maria, 990);
-
-            //Act - método sob teste
-            leilao.TerminaPregao();
-
-            //Assert - verificação dos critérios de aceitação (verifica se o Act foi bem sucedido)
-            var valorEsperado = 1000;
-            var valorObtido = leilao.Ganhador.Valor;
-
-            Assert.Equal(valorEsperado, valorObtido);
-
-        }
-
-        [Fact]
-        public void LeilaoComApenasUmLance()
-        {
-            //Arrange - cenário
-            var leilao = new Leilao("Van Gogh");
-            var fulano = new Interessada("Fulano", leilao);
-
-            leilao.RecebeLance(fulano, 800);
-
-            //Act - método sob teste
-            leilao.TerminaPregao();
-
-            //Assert - verificação dos critérios de aceitação (verifica se o Act foi bem sucedido)
-            var valorEsperado = 800;
             var valorObtido = leilao.Ganhador.Valor;
 
             Assert.Equal(valorEsperado, valorObtido);
