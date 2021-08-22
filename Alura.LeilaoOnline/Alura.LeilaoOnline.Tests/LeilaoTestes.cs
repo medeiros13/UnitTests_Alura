ad2e303e0359738c8f147d93a00ee3ece63e0729
medeiros13 <gabrielmedeiros13122000@gmail.com>
2021-08-22 17:44:46 -0300

    Criado primeiro projeto de testes do xUnit

---
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTestes.cs
@@ -0,0 +1,55 @@
+﻿using Alura.LeilaoOnline.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LeilaoTestes
+    {
+        [Fact]
+        public void LeilaoComVariosLances()
+        {
+            //Arrange - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(maria, 990);
+
+            //Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert - verificação dos critérios de aceitação (verifica se o Act foi bem sucedido)
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Assert.Equal(valorEsperado, valorObtido);
+
+        }
+        [Fact]
+        public void LeilaoComApenasUmLance()
+        {
+            //Arrange - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+
+            //Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert - verificação dos critérios de aceitação (verifica se o Act foi bem sucedido)
+            var valorEsperado = 800;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+    }
+}
