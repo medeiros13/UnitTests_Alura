6096b00a70100c2e19e38708f755ef61a7ad01e2
medeiros13 <gabrielmedeiros13122000@gmail.com>
2021-10-30 00:35:44 -0300

    Adicionado teste que verifica se o valor do lance é negativo

---
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LanceConstrutor.cs
@@ -0,0 +1,24 @@
+﻿using Alura.LeilaoOnline.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LanceConstrutor
+    {
+        [Fact]
+        public void LancaArgumentExceptionDadoValorNegativo()
+        {
+            //Arrange
+            var valorNegativo = -100;
+
+            //Assert
+            Assert.Throws<ArgumentException>(
+                //Act
+                () => new Lance(null, valorNegativo)
+            );
+        }
+    }
+}
